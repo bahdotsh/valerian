ba840f8e9be5e37bcde0cddbbc087a4f9d03023a
bahdotsh <appu.yess@gmail.com>
2023-05-02 15:21:10 +0530

    test based approach parsing numbers

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+#[derive(Debug, PartialEq)]
+pub struct Number(pub i32);
+
+impl Number {
+    pub fn new(s: &str) -> Self {
+        Self(s.parse().unwrap())
+    }
+}
+
+fn main() {}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn psrse_number() {
+        assert_eq!(Number::new("123"), Number(123));
+    }
 }
