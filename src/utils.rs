7fad307da9de9b4c8cc2e5cbc855a56254161f45
bahdotsh <appu.yess@gmail.com>
2023-05-02 15:56:01 +0530

    added tests for multiple digits extraction

--- src/utils.rs
+++ src/utils.rs
@@ -1,5 +1,13 @@
 pub(crate) fn extract_digits(s: &str) -> (&str, &str) {
-    (&s[1..], &s[0..1])
+    let digits_end = s
+        .char_indices()
+        .find_map(|(idx, c)| if c.is_ascii_digit() {None} else { Some(idx) })
+        .unwrap_or_else(|| s.len());
+
+    let digits = &s[..digits_end];
+    let remainder = &s[digits_end..];
+
+    ( remainder, digits )
 } 
 
 #[cfg(test)]
@@ -9,6 +17,16 @@ mod tests{
     #[test]
     fn extract_one_digit(){
         assert_eq!(extract_digits("1+2"), ("+2", "1"));
+    }
+
+    #[test]
+    fn not_extracting_from_empty() {
+        assert_eq!(extract_digits(""), ("", ""));
+    }
+
+    #[test]
+    fn extract_multiple() {
+        assert_eq!(extract_digits("100+12"), ("+12", "100") );
     }
 
 }
