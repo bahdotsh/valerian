64ab869374335b572ec12eb9fac933212abbc3dd
bahdotsh <appu.yess@gmail.com>
2023-05-02 15:31:29 +0530

    parsing operators

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,26 @@ impl Number {
     }
 }
 
+#[derive(Debug, PartialEq)]
+pub enum Op {
+    Add,
+    Sub,
+    Mul,
+    Div,
+}
+
+impl Op {
+    pub fn new(op: &str) -> Self {
+        match op {
+            "+" => Op::Add,
+            "-" => Op::Sub,
+            "*" => Op::Mul,
+            "/" => Op::Div,
+            _ => panic!("Bad Operator"),
+        }
+    }
+}
+
 fn main() {}
 
 #[cfg(test)]
@@ -14,7 +34,27 @@ mod tests {
     use super::*;
 
     #[test]
-    fn psrse_number() {
+    fn parse_number() {
         assert_eq!(Number::new("123"), Number(123));
+    }
+
+    #[test]
+    fn parse_add_op() {
+        assert_eq!(Op::new("+"), Op::Add);
+    }
+
+    #[test]
+    fn parse_sub_op() {
+        assert_eq!(Op::new("-"), Op::Sub);
+    }
+
+    #[test]
+    fn parse_mul_op() {
+        assert_eq!(Op::new("*"), Op::Mul);
+    }
+
+    #[test]
+    fn parse_div_op() {
+        assert_eq!(Op::new("/"), Op::Div);
     }
 }
